7471d8d8a1069a3b685b39f94c91ee9a73db7eba
SorenAAU <sesc24@student.aau.dk>
2025-09-29 11:21:20 +0200

    Testing CI

--- src/main/java/com/aau/p3/App.java
+++ src/main/java/com/aau/p3/App.java
@@ -6,8 +6,9 @@ package com.aau.p3;
  */
 public class App 
 {
+    public static int age = 23;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! " + age);
     }
 }
