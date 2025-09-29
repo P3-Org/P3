f707e21aeb6c9eb141c66b2696ffcdc9d2d108d9
Søren Emil Schmidt-Laugesen <sesc24@student.aau.dk>
2025-09-29 23:17:35 +0200

    Maven  setup + Test test

---
+++ src/main/java/com/aau/p3/Calculator.java
@@ -0,0 +1,11 @@
+package com.aau.p3;
+
+public class Calculator {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/com/aau/p3/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.aau.p3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    @DisplayName("Add two integers")
+    void add() {
+        assertEquals(4,Calculator.add(2,2));
+    }
+
+    @Test
+    @DisplayName("Multiply tow integers")
+    void multiply() {
+        assertAll(
+            () -> assertEquals(10,Calculator.multiply(5,2)),
+            () -> assertEquals(100,Calculator.multiply(10,10)));
+    }
+}
