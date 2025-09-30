f4e1c7c6dcadce5dbbd469f707c9fd1fef417ca7
Søren Emil Schmidt-Laugesen <sesc24@student.aau.dk>
2025-09-30 11:52:56 +0200

    Fixed format

--- src/main/java/com/aau/p3/Calculator.java
+++ src/main/java/com/aau/p3/Calculator.java
@@ -5,8 +5,12 @@ public class Calculator {
     static int add(int a, int b) {
         return a + b;
     }
+
     static int multiply(int a, int b) {
         return a * b;
     }
-    static int divide(int a, int b) { return a / b;}
+
+    static int divide(int a, int b) {
+        return a / b;
+    }
 }
