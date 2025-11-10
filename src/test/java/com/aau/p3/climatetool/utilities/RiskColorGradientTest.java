d65b49b780262b4a2bd1a0ebe4f23d06c687ebc7
FrederikSperling <frederik.sperling32121@gmail.com>
2025-11-10 14:35:06 +0100

    test setup

---
+++ src/test/java/com/aau/p3/climatetool/utilities/RiskColorGradientTest.java
@@ -0,0 +1,13 @@
+package com.aau.p3.climatetool.utilities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RiskColorGradientTest {
+
+    @Test
+    void assignColors() {
+       // RiskColorGradient.assignColors()
+    }
+}
