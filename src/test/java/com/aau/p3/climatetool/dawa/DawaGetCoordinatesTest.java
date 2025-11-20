a6232c13e32c5541100255d714dd40ce8fc7a0d3
DrMachoo <davitootooop@gmail.com>
2025-11-20 11:41:21 +0100

    Fixed testing

--- src/test/java/com/aau/p3/climatetool/dawa/DawaGetCoordinatesTest.java
+++ src/test/java/com/aau/p3/climatetool/dawa/DawaGetCoordinatesTest.java
@@ -1,7 +1,6 @@
-package com.aau.p3.dawa;
+package com.aau.p3.climatetool.dawa;
 
-import com.aau.p3.climatetool.dawa.DawaAutocomplete;
-import com.aau.p3.climatetool.dawa.DawaGetCoordinates;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
